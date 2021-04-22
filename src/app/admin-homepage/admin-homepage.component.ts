8ac3ed375c9744976a3e05e7a5e64b019230d3a7
Muskaan <muskaanpansare24@gmail.com>
2021-04-22 10:53:20 +0530

    Created Customer and Retailer model

--- src/app/admin-homepage/admin-homepage.component.ts
+++ src/app/admin-homepage/admin-homepage.component.ts
@@ -2,6 +2,7 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component } from '@angular/core';
 import { Admin } from '../appmodel/admin';
 import { AdminService } from '../admin.service';
+import { CustomerService } from '../customer.service';
 
 
 @Component({
@@ -20,7 +21,11 @@ export class AdminHomepageComponent {
   adminId: number;
   admin: Admin = new Admin();
 
-  open(content) {
+
+
+   
+//  This is model for profile
+  openProfile(content) {
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
@@ -44,6 +49,73 @@ export class AdminHomepageComponent {
       return `with: ${reason}`;
     }
   }
+
+
+
+  // This is for all Customer 
+  openTotalCustomer(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReasonCustomer(reason)}`;
+    });
+
+    // this is to get the admin information
+    this.adminId = parseInt(sessionStorage.getItem('adminId'));
+    this.adminService.fetchProfile(this.adminId).subscribe(response => {
+      this.admin = response;
+    })
+
+  }
+
+  private getDismissReasonCustomer(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+
+//  This is for Total Retailer 
+  openTotalRetailer(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReasonRetailer(reason)}`;
+    });
+
+    // this is to get the admin information
+    this.adminId = parseInt(sessionStorage.getItem('adminId'));
+    this.adminService.fetchProfile(this.adminId).subscribe(response => {
+      this.admin = response;
+    })
+
+  }
+
+  private getDismissReasonRetailer(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
